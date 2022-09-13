d964f1db6fe15336e80d33858b4790382a8cf3fc
julyprince <princejec8080@gmail.com>
2022-09-13 12:07:34 -0400

    directiva *ngFor basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,16 @@ export class AppComponent {
     this.registrado=true;
     this.mensaje="usuario registrado"
   }
+
+  /*ng for*/
+  entradas:Array<{titulo:string}>;
+  constructor(){
+    this.entradas =[
+      {titulo:"python cada dia mas presente"},
+      {titulo:"java presente desde mas de 20 anios"},
+      {titulo:"java script cada vez mas funcional"},
+      {titulo:"kotlin potencia para tus apps"},
+      {titulo:"donde quedo pascal?"}
+    ]
+  }
 }
